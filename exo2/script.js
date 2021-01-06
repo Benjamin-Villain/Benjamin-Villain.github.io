7876e9226266ba0ec26b3c956d9bcc023192a708
Benjamin-Villain <benjaminnicolasvillain@gmail.com>
2021-01-06 12:00:43 +0100

    exo2 suite

--- exo2/script.js
+++ exo2/script.js
@@ -1,7 +1,32 @@
-$(document).ready( function() {
+
+var geo_options = {
+    enableHighAccuracy: true,
+    maximumAge        : 0,
+    timeout           : 10000
+  };
+
+function testGeoLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition();
-        }
-        
-    });
-watchPosition();
+        /* la géolocalisation est disponible */
+        console.log("la géolocalisation est disponible")
+      } else {
+        alert("la géolocalisation est indisponible")
+      }
+}
+
+navigator.geolocation.getCurrentPosition(function(pos) {
+    let crd = pos.coords;
+    document.getElementById("long").innerHTML = crd.longitude;
+    document.getElementById("lat").innerHTML = crd.latitude;
+    document.getElementById("prec").innerHTML = crd.accuracy;
+    document.getElementById("vit").innerHTML = crd.speed;
+    document.getElementById("ts").innerHTML = pos.timestamp;
+});
+wp = navigator.geolocation.watchPosition(function(pos) {
+    let crd = pos.coords;
+    document.getElementById("long").innerHTML = crd.longitude;
+    document.getElementById("lat").innerHTML = crd.latitude;
+    document.getElementById("prec").innerHTML = crd.accuracy;
+    document.getElementById("vit").innerHTML = crd.speed;
+    document.getElementById("ts").innerHTML = pos.timestamp
+});
